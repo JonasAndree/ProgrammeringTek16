9e1541ea9ddf145945b84bd6d85d30583a173bfc
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2017-11-24 10:40:39 +0100

    Poop icon

---
+++ src/MyFirstGame/MultipleChoises.java
@@ -0,0 +1,64 @@
+package MyFirstGame;
+
+import java.util.Random;
+import javax.swing.ImageIcon;
+import javax.swing.JOptionPane;
+
+public class MultipleChoises {
+
+	public static void main(String[] args) {
+
+		// En lista med text i!
+		String[] options = new String[] { "Role the dice.", "Leave", "Look at it.", "Ask the man infront." };
+		// Hakparantes för att hämta en position.
+		String option3 = options[3];
+		// Bilden
+		ImageIcon diceIcon = new ImageIcon("C:/Users/jonas.andree/Desktop/dice.png");
+		ImageIcon rulle = new ImageIcon("C:/Users/jonas.andree/Desktop/pargment/0003.png");
+		ImageIcon leaveIcon = new ImageIcon("C:/Users/jonas.andree/Desktop/leave.png");
+		ImageIcon poopIcon = new ImageIcon("C:/Users/jonas.andree/Desktop/poop.jpeg");
+		// Random tal generator.
+		Random diceRole = new Random();
+		// Saves responce to an integer.
+		int response = JOptionPane.showOptionDialog(null, "You have reached a dungeon \nWhat would you like to do?",
+				"DUNGEON ENTRENCE!!!!!!", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options,
+				options[1]);
+
+		
+		if (response == 0) {
+			int diceValue = diceRole.nextInt(6);
+			JOptionPane.showMessageDialog(null,
+					"You role the dice " + "\nAnd you roled a: " + diceValue, "Dice", -1, diceIcon);
+		} else if (response == 1) {
+			JOptionPane.showMessageDialog(null, "You selected leave! ", "Leave", -1, leaveIcon);
+		} else {
+			JOptionPane.showMessageDialog(null, "You selected " + options[response], "You selected", -1);
+		}
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		String[] choices = { "Sword", "Pipe", "Gold", "Poop", "Banana", "Grain" };
+		
+		
+		String input = (String) JOptionPane.showInputDialog(null, "Choose now...", "The Choice of a LIFETIME!!!!!!",
+				JOptionPane.QUESTION_MESSAGE, null, // Use
+													// default
+													// icon
+				choices, // Array of choices
+				choices[3]); // Initial choice
+
+		if (input.equals(choices[3]))
+			JOptionPane.showMessageDialog(null, "You selected " + input, "Poop!!", -1, poopIcon);
+		else
+			JOptionPane.showMessageDialog(null, input);
+
+	}
+
+}
