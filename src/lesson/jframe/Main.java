ba362f2ac04173e64882138625bb408ee7f1d3b7
JonasAndree <31105305+JonasAndree@users.noreply.github.com>
2018-02-14 09:01:47 +0100

    pre jframe

--- src/lesson/jframe/Main.java
+++ src/lesson/jframe/Main.java
@@ -2,6 +2,7 @@ package lesson.jframe;
 
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Toolkit;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
@@ -18,7 +19,9 @@ public class Main extends JFrame implements WindowListener {
 		// Lägger till sjig sjålv som en lysner till sitt egna fönster
 		this.addWindowListener(this);
 		
-		this.setSize(500, 500);
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		
+		this.setSize(tk.getScreenSize().width, tk.getScreenSize().height);
 		this.setUndecorated(true);
 		this.setOpacity(0.95f);
 		this.setLocationRelativeTo(null);
@@ -29,17 +32,41 @@ public class Main extends JFrame implements WindowListener {
 	
 	public void addButtons(Container contentPane) {
 		// Create a  JButton
-		JButton okButton = new JButton("Jacob was here!");
-		
-		contentPane.add(okButton, BorderLayout.CENTER);
-		
+		JButton okButton = new JButton("OK");
+		contentPane.add(okButton, BorderLayout.SOUTH);
 		okButton.addActionListener(e -> buttonAction());
+		
+
+		JButton closeButton = new JButton("Close");
+		contentPane.add(closeButton, BorderLayout.NORTH);
+		closeButton.addActionListener(e -> buttonActionClose());
+
+
+		JButton nextButton = new JButton("Next");
+		contentPane.add(nextButton, BorderLayout.EAST);
+		nextButton.addActionListener(e -> buttonActionNext());
+
+
+		JButton backButton = new JButton("Back");
+		contentPane.add(backButton, BorderLayout.WEST);
+		backButton.addActionListener(e -> buttonActionBack());
+		
+		
 	}
-	
+
+	public void buttonActionNext() {
+		System.out.println("Next Button pressed!");
+	}
+	public void buttonActionBack() {
+		System.out.println("Back Button pressed!");
+	}
 	public void buttonAction() {
-		System.out.println("Aymaan is opressed!!!");
+		System.out.println("Ok Button pressed!");
 	}
-	
+	public void buttonActionClose() {
+		System.out.println("Close Button pressed!");
+		System.exit(0);
+	}
 	
 	public static void main(String[] args) {
 		Main frame = new Main();
